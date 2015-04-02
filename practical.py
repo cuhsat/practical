3c259fe6fc07c381c1d2eb91c6d9d99cac001985
Christian Uhsat <christian@uhsat.de>
2015-04-02 11:15:21 +0200

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,1 +1,156 @@
+#!/usr/bin/env python
+"""
+This is free and unencumbered software released into the public domain.
 
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+"""
+import os
+import re
+import sys
+
+
+__all__, __version__ = ["Practical"], "0.1.0"
+
+
+class Practical(object):
+    """
+    The Practical Cipher.
+    """
+    def __init__(self, table=None):
+        """
+        Sets the used conversion table.
+        """
+        self.table = table or [
+            ["A", "B", "C", "D", "E", "F"],
+            ["G", "H", "I", "J", "K", "L"],
+            ["M", "N", "O", "P", "Q", "R"],
+            ["S", "T", "U", "V", "W", "X"],
+            ["Y", "Z", "0", "1", "2", "3"],
+            ["4", "5", "6", "7", "8", "9"]
+        ]
+
+    def __crypt(self, text, key, mode):
+        """
+        Returns the crypted text.
+        """
+        text = re.sub("[^A-Z0-9]", "", text.upper())
+        key  = re.sub("[^A-Z0-9]", "", key.upper())
+
+        if len(text) != len(key):
+            raise Exception("Key length does not match")
+
+        def lookup(symbol, table="".join([x for y in self.table for x in y])):
+            i = table.index(symbol)
+            x = i % 6
+            y = i / 6
+
+            return (x, y)
+
+        output = ""
+
+        for i in range(len(text)):
+            tx, ty = lookup(text[i])
+            kx, ky = lookup(key[i])
+
+            rx = mode(tx, kx) % 6
+            ry = mode(ty, ky) % 6
+
+            output += self.table[ry][rx]
+
+        return output
+
+    def encrypt(self, text, key):
+        """
+        Returns the encrypted text.
+        """
+        return self.__crypt(text, key, lambda x, y: (x + y))
+
+    def decrypt(self, text, key):
+        """
+        Returns the decrypted text.
+        """
+        return self.__crypt(text, key, lambda x, y: (x - y))
+
+    def generate(self, size):
+        """
+        Returns a new random key block.
+        """
+        block = ""
+
+        for i in range(size):
+            x = sum([ord(n) for n in os.urandom(6)]) % 6
+            y = sum([ord(n) for n in os.urandom(6)]) % 6
+
+            block += self.table[y][x]
+
+        return block
+
+
+def main(script, command="--help", key=None, *text):
+    """
+    Usage: %s COMMAND [KEY TEXT...]
+
+    Commands:
+      -e --encrypt    Encrypts the given text
+      -d --decrypt    Decrypts the given text
+      -g --generate   Generates a random key block
+
+      -h --help       Shows this text
+      -l --license    Shows the license
+      -v --version    Shows the version
+
+    Report bugs to <christian@uhsat.de>
+    """
+    try:
+        script = os.path.basename(script)
+
+        if command in ("/?", "-h", "--help"):
+            print(re.sub("(?m)^ {4}", "", main.__doc__ % script).strip())
+
+        elif command in ("-l", "--license"):
+            print(__doc__.strip())
+
+        elif command in ("-v", "--version"):
+            print("Practical Cipher " + __version__)
+
+        elif command in ("-e", "--encrypt") and key and text:
+            print(Practical().encrypt(" ".join(text), key))
+
+        elif command in ("-d", "--decrypt") and key and text:
+            print(Practical().decrypt(" ".join(text), key))
+
+        elif command in ("-g", "--generate"):
+            print(Practical().generate())
+
+        else:
+            print("Unknown command or parameter not given.")
+            print("Please use --help for help on commands.")
+            return 2 # Incorrect usage
+
+    except Exception as ex:
+        return "%s error: %s" % (script, ex)
+
+
+if __name__ == "__main__":
+    sys.exit(main(*sys.argv))
